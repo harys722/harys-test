c85207ccea0f0f451d5e2c3c955a8e9740aabb03
- HARYs - <174037813+harys722@users.noreply.github.com>
2025-06-01 20:53:50 +0500

    encode.js

--- api/encode.js
+++ api/encode.js
@@ -1,21 +1,14 @@
-const express = require('express');
-const app = express();
-const port = 3000; 
-
-app.get('/encode', (req, res) => {
-  const htmlToEncode = req.query.html;
+export default function handler(request, response) {
+  const htmlToEncode = request.query.html;
   if (!htmlToEncode) {
-    return res.status(400).send('Missing html parameter');
+    response.status(400).send('Missing html parameter');
+    return;
   }
 
   try {
     const encodedHtml = Buffer.from(htmlToEncode).toString('base64');
-    res.send(encodedHtml);
+    response.send(encodedHtml);
   } catch (error) {
-    res.status(500).send('Error encoding HTML');
+    response.status(500).send('Error encoding HTML');
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+}
